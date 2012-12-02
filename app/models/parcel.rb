4ac0c533dcfec4a812b8596294bae96005b72ec1
Joel Mahoney <joelmahoney@gmail.com>
2012-12-01 19:15:14 -0800

    Add geocode_address method to parcels

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -1,3 +1,13 @@
 class Parcel < ActiveRecord::Base
   has_many :schools
+  before_save :geocode_address
+  
+  private 
+  
+  def geocode_address
+    # boston_bounds = Geokit::Geocoders::GoogleGeocoder.geocode('Boston, MA').suggested_bounds
+    geo = Geokit::Geocoders::MultiGeocoder.geocode("#{address}, Boston, MA, #{zipcode}")
+    errors.add(:address, "Could not Geocode address") if !geo.success
+    self.lat, self.lng = geo.lat,geo.lng if geo.success
+  end
 end
